bdc20da5964dcf35f375808977e576b7e70fa286
Vincent Schauer <schauervincent@gmail.com>
2020-03-24 21:32:15 +0100

    splash screen

---
+++ app/src/main/java/com/example/raco/SplashScreen.kt
@@ -0,0 +1,18 @@
+package com.example.raco
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //perform login checks etc.
+
+
+
+        startActivity(Intent(this, MainActivity::class.java))    }
+}
