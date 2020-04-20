61587bf65dc67eda63676d2a1ce3b95bde6f91ff
Vincent Schauer <schauervincent@gmail.com>
2020-04-20 17:45:05 +0200

    teile von login/register

--- app/src/main/java/com/example/raco/login/LoginFragment.kt
+++ app/src/main/java/com/example/raco/login/LoginFragment.kt
@@ -1,20 +1,35 @@
 package com.example.raco.login
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.raco.R
 import com.example.raco.navigationDrawerActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.core.Tag
 import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class LoginFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+    }
+
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +38,7 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -39,5 +55,8 @@ class LoginFragment : Fragment() {
             findNavController().navigate(R.id.action_LoginFragment_to_resetPasswordFragment)
         }
     }
+
+
+
 
 }
--- app/src/main/java/com/example/raco/login/RegisterFragment.kt
+++ app/src/main/java/com/example/raco/login/RegisterFragment.kt
@@ -1,20 +1,35 @@
 package com.example.raco.login
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.raco.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.fragment_register.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class RegisterFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        button_register.setOnClickListener {
+            createAccount(email.text.toString(), password_one.text.toString())
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,7 +39,11 @@ class RegisterFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
-
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -59,5 +78,30 @@ class RegisterFragment : Fragment() {
             }
         }
     }
+
+    private fun createAccount (email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(ContentValues.TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(ContentValues.TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(activity, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        //hier dann zur appropriate activity/fraggment weiterleiten?
+
+    }
+
 
 }
